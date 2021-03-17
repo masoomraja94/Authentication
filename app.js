4a535c2d4ccab1522e45afcb168d88b69e9cb06b
Masoom Raja Ansari <masoomraja94@gmail.com>
2021-03-17 14:22:44 +0530

    Added bcrypt hashing

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const ejs = require('ejs');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 // const encrypt = require('mongoose-encryption');
-const md5 = require('md5');
+// const md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds=10;
 
 const app=express();
 
@@ -44,7 +46,7 @@ app.get("/login",function (req,res){
 
 app.post("/login",function (req,res){
   const username=req.body.username;
-  const password=md5(req.body.password);
+  //const password=md5(req.body.password);
 
   User.findOne({email:username},function (err,foundUser){
     if(err){
@@ -52,12 +54,21 @@ app.post("/login",function (req,res){
     }
     else{
       if(foundUser){
-        if(foundUser.password==password){
-          res.render("secrets");
-        }
-        else{
-          res.send("Incorrect password.");
-        }
+        // if(foundUser.password==password){
+        //   res.render("secrets");
+        // }
+        // else{
+        //   res.send("Incorrect password.");
+        // }
+
+        bcrypt.compare(req.body.password,foundUser.password,function (err,result){
+          if(result==true){
+            res.render("secrets");
+          }
+          else{
+            res.send("Incorrect password.");
+          }
+        });
       }
       else{
         res.send("Check username and password.");
@@ -73,18 +84,37 @@ app.get("/register",function (req,res){
 });
 
 app.post("/register",function (req,res){
-  const newUser=new User({
-    email:req.body.username,
-    password:md5(req.body.password)
+
+  /* bcrypt hashing used */
+  bcrypt.hash(req.body.password,saltRounds,function (err,hash){
+    // err ignored here.
+    const newUser=new User({
+      email:req.body.username,
+      password:hash
+    });
+    newUser.save(function (err){
+      if(err){
+        res.send(err);
+      }
+      else{
+        res.render("secrets");
+      }
+    });
   });
-  newUser.save(function (err){
-    if(err){
-      res.send(err);
-    }
-    else{
-      res.render("secrets");
-    }
-  });
+
+  /* md5 hashing used */
+  // const newUser=new User({
+  //   email:req.body.username,
+  //   password:md5(req.body.password)
+  // });
+  // newUser.save(function (err){
+  //   if(err){
+  //     res.send(err);
+  //   }
+  //   else{
+  //     res.render("secrets");
+  //   }
+  // });
 });
 
 
